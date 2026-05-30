23f09c82f4c81babf2791fbc356a42e49915b5f0
Vigan Sadiku <vigansadiku2003@gmail.com>
2026-05-30 17:03:53 +0200

    Upated NavBar with auto state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,22 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
-
-// TODO: import and use AuthContext here to show conditional links
+import { AuthContext } from '../context/AuthContext'
 
 function Navbar() {
+  const { user, logout } = useContext(AuthContext)
+
   return (
     <nav className="bg-gray-900 text-white px-6 py-4 flex items-center justify-between">
       <Link to="/" className="font-bold text-lg">Movie Watchlist</Link>
-      <div className="flex gap-4 text-sm">
-        {/* TODO: if no user is logged in, show a Login link */}
-        {/* TODO: if a user is logged in, show a Welcome message and a Logout button */}
-        <Link to="/login" className="hover:underline">Login</Link>
+      <div className="flex gap-4 text-sm items-center">
+        {user ? (
+          <>
+            <span>Welcome, {user.username}!</span>
+            <button onClick={logout} className="hover:underline">Logout</button>
+          </>
+        ) : (
+          <Link to="/login" className="hover:underline">Login</Link>
+        )}
       </div>
     </nav>
   )
